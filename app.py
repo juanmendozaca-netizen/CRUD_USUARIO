200aa29dd1e106e580fa953367548c006cc8c865
Juan Carlos Mendoza <juan.mendoza.ca@tecsup.edu.pe>
2026-03-24 19:36:16 -0500

    actualizado

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("postgres://"):
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     
+
 def get_conn():
     return psycopg2.connect(DATABASE_URL)
 
